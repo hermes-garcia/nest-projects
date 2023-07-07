b9dee9d3c700f617c056b26003df0e0e16004e9f
Hermes <hermes.garcia@bounteous.com>
2023-07-06 21:56:43 -0600

    Pokedex - Create axios adapter to http injectable provider

--- 03-pokedex/src/seed/seed.module.ts
+++ 03-pokedex/src/seed/seed.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
-import { Pokemon, PokemonSchema } from '../pokemon/entities/pokemon.entity';
 import { PokemonModule } from '../pokemon/pokemon.module';
+import { CommonModule } from '../common/common.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
-  imports: [PokemonModule],
+  imports: [PokemonModule, CommonModule],
 })
 export class SeedModule {}
--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,18 +1,19 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import axios from 'axios';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { PokeapiResponse } from './interfaces/pokeapi-response.interface';
 import { Pokemon } from '../pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
   async populateDatabase() {
-    const { data } = await axios.get<PokeapiResponse>(
+    const data = await this.http.get<PokeapiResponse>(
       `https://pokeapi.co/api/v2/pokemon?limit=493`,
     );
     const pokemonToInsert: { name: string; no: number }[] = [];
